d0cd8bbbccf574121a8afa7ac7d4425a8561d186
Abhishek khare <ghostabhi323@gmail.com>
2024-12-05 08:49:12 +0530

    feat: Add Heading component for page titles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
 import React from "react"
 import MaxWidthWrapper from "./components/MaxWidthWrapper"
+import Heading from "./components/Heading"
 
 const Page = () => {
   return (
     <>
       <section className="relative py-24 sm:py-32 bg-brand-25">
         <MaxWidthWrapper className="text-center">
-          <div className="relative mx-auto text-center flex flex-col items-center gap-10"></div>
+          <div className="relative mx-auto text-center flex flex-col items-center gap-10">
+            <div>
+              <Heading>
+                <span>Real-Time SaaS Insights,</span>
+                <br />
+                <span className="relative bg-gradient-to-r from-brand-700 to-brand-800 text-transparent bg-clip-text">
+                  Delivered to You Discord
+                </span>
+              </Heading>
+            </div>
+          </div>
         </MaxWidthWrapper>
       </section>
       <section></section>
