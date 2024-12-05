3244976ec0ebe9cd7db068abddc4f1cb278469dc
Abhishek khare <ghostabhi323@gmail.com>
2024-12-05 21:26:05 +0530

    feat: Add ShinyButton component for call-to-action buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import MaxWidthWrapper from "./components/MaxWidthWrapper"
 import Heading from "./components/Heading"
 import { Check } from "lucide-react"
+import ShinyButton from "./components/ShinyButton"
 
 const Page = () => {
   return (
@@ -39,6 +40,15 @@ const Page = () => {
                 </li>
               ))}
             </ul>
+
+            <div className="w-full max-w-80 ">
+              <ShinyButton
+                href="/sign-up"
+                className="relative z-10 h-14 w-full text-base shadow-lg transition-shadow duration-300 hover:shadow-xl"
+              >
+                Start For Free Today
+              </ShinyButton>
+            </div>
           </div>
         </MaxWidthWrapper>
       </section>
